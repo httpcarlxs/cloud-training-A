96f489f6f76dbb48e2f9e269ffd156004a1c9fbf
Carlos Fuch <httpcarlosx@gmail.com>
2023-07-31 13:16:53 -0300

    appA.v0

---
+++ aplicacaoA.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+import logging
+
+app = Flask(__name__)
+
+logging.basicConfig(filename='aplicacao_a.log', level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+
+@app.route('/endpoint', methods=['POST'])
+def endpoint_handler():
+    data = request.json
+    logging.info(f"Requisição recebida: {data}")
+    return True
+
+if __name__ == '__main__':
+    app.run(host='10.11.19.192', port=5000)
